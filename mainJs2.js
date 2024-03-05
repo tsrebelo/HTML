292daf943a4664ee68d194f28f6b9e0144c79b89
telma <145616594+tsrebelo@users.noreply.github.com>
2024-03-05 12:41:22 -0100

    Add files via upload

---
+++ mainJs2.js
@@ -0,0 +1,25 @@
+/**
+*Código principal de jQuery
+*/
+/**
+ * Para selecionar um elemento em jQuery
+ * $(elemento)
+ * 
+ * Para selecionar um documento todo
+ * $(document)
+ * 
+ * Para selecionar tudo dentro do body
+ * $("body")
+ * 
+ * Para selecionar um id (exemplo id = "main")
+ * $("#main")
+ * 
+ * Para selecionar uma class (exemplo class = "row")
+ * $(".row")
+ */
+$(document).ready(function(){
+    console.log("O documento está pronto");
+
+    
+
+});
