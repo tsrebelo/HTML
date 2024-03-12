2970907edf7b122982fd9c7abefb50a275d88a0c
Telma Rebelo <rebelotelma01@gmail.com>
2024-03-12 12:28:02 -0100

    updates

--- javaScript/mainJs1.js
+++ javaScript/mainJs1.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
         var username = $("input#username").val();
         var email = $("input#email").val();
         var birthday = $("input#nascimento").val();
-        var frase = "Olá "+name+", o seu user name "+username+" com data de nascimento "+birthday+", foi registado para o email "+email+"!!!";
+        var frase = "Olá "+name+", o seu user name "+username+" com data de nascimento "+birthday+", foi registado para o email "+email+".";
         console.log(frase);
         //prevenir sçso default = que a pagina seja recarregada
         event.preventDefault();
@@ -45,6 +45,7 @@ $(document).ready(function(){
         $("#loader").fadeIn(5000, function(){ //milisegundos do fadeÎn
             console.log("fadeIn completo");
             $(this).find("span").text("Registo efetuado.");
+            $(this).append("<p>"+frase+"<p>");
             //funcionamento do botao fechar
             $(this).find('.close').on("click", function(){
                 $("#loader").fadeOut(500);
