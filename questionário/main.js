21311efaea9e64b21bbd37699e98dbe4641a799d
Telma Rebelo <rebelotelma01@gmail.com>
2024-03-13 12:14:10 -0100

    updates

--- questionário/main.js
+++ questionário/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function()){
-    
-}
+$(document).ready(function(){
+    $("form").on("submit", function(event){
+        //prevenir que pagina do action seja carregada
+        event.preventDefault();
+        //guarda array com respostas selecionadas
+        var answers = $("input:checked");
+        var matrix = {
+            "quest1": "bmw",
+            "quest2": "francesinha",
+            "quest3": "vav",
+            "quest4": "mini",
+            "quest5": "agua",
+            "quest6": "honda"
+        };
+    });
+});
