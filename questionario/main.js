da5c56615d898747e4a78d87b3444aed5b6da538
Telma Rebelo <rebelotelma01@gmail.com>
2024-03-14 12:24:55 -0100

    updates

--- questionario/main.js
+++ questionario/main.js
@@ -13,17 +13,24 @@ $(document).ready(function(){
             "quest5": "agua",
             "quest6": "honda"
         };
+        var correct = 0;
+        var wrong = 0;
         answers.each(function(){
             var thisV = $(this);
             var quest = thisV[0]["name"];
             var answers = thisV[0]["id"];
-            console.log(name +" - "+ id);
+            console.log(quest +" - "+ answers);
             //validar se as respostas correspondem a matrix
             if(matrix[quest] == answers){
                 console.log("CORRETO");
+                correct++;
+                thisV.addClass("correct");
             } else {
                 console.log("ERRADO");
+                wrong++;
+                thisV.addClass("wrong");
             }
         });
+        $("form").append("Tem "+correct+" perguntas certas"+" e "+wrong+" peruntas erradas.");
     });
 });     
